0362f65f92ce9485d76abf8a44c6334ae218e1f6
Isaac  Tingo <Isaac.Tingo@jubileekenya.com>
2022-09-19 14:43:08 +0300

    Update profile

--- models/user.js
+++ models/user.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    phoneNumber: Number,
-    image1: String,
-    image2: String,
-    image3: String,
-    location: String,
-    rate: Number,
-    bio: String,
-    password: String,
+  firstName: String,
+  lastName: String,
+  phoneNumber: Number,
+  password: String,
 
-    rating: Number,
-    isFeatured: Boolean,
-    isVerified: Boolean,
-    paidSignUp: Boolean
-})
+  image1: String,
+  image2: String,
+  image3: String,
+  location: String,
+  rate: Number,
+  bio: String,
 
-exports.User = mongoose.model("User", UserSchema)
+  rating: Number,
+  isFeatured: Boolean,
+  isVerified: Boolean,
+  paidSignUp: Boolean,
+});
+
+exports.User = mongoose.model("User", UserSchema);
--- routes/user.js
+++ routes/user.js
@@ -4,47 +4,156 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const { firstName, lastName, phoneNumber, password } = req.body;
-  await User.findOne({ phoneNumber })
-    .then(async (response) => {
-      if (response) {
-        //User exists
+
+  if (!firstName) {
+    res.json({
+      status: "Failed",
+      message: "First name is required",
+    });
+  } else if (!lastName) {
+    res.json({
+      status: "Failed",
+      message: "Last name is required",
+    });
+  } else if (!phoneNumber) {
+    res.json({
+      status: "Failed",
+      message: "Phone number is required",
+    });
+  } else if (!password) {
+    res.json({
+      status: "Failed",
+      message: "Password is require",
+    });
+  } else {
+    await User.findOne({ phoneNumber })
+      .then(async (response) => {
+        if (response) {
+          //User exists
+          res.json({
+            status: "Failed",
+            message: "User with the given phone number exists",
+          });
+        } else {
+          //User doesnt exist
+          const newUser = new User({
+            firstName,
+            lastName,
+            phoneNumber,
+            password,
+            image1: "",
+            image2: "",
+            image3: "",
+            location: "",
+            rate: 0,
+            bio: "",
+
+            rating: 0,
+            isFeatured: false,
+            isVerified: false,
+            paidSignUp: false,
+          });
+
+          await newUser
+            .save()
+            .then(() => {
+              res.json({
+                status: "Success",
+                message: "Account created successfully",
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              res.json({
+                status: "Failed",
+                message: "Error occured while saving user data",
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
         res.json({
           status: "Failed",
-          message: "User with the given phone number exists",
+          message: "Error occured while getting user data",
         });
-      } else {
-        //User doesnt exist
-        const newUser = new User({
-          firstName,
-          lastName,
-          phoneNumber,
-          password,
-        });
+      });
+  }
+});
 
-        await newUser
-          .save()
+router.post("/registration", async (req, res) => {
+  const { phoneNumber, image1, image2, image3, location, rate, bio } = req.body;
+  if (!image1) {
+    res.json({
+      status: "Failed",
+      message: "3 photos are required",
+    });
+  } else if (!image2) {
+    res.json({
+      status: "Failed",
+      message: "3 photos are required",
+    });
+  } else if (!image3) {
+    res.json({
+      status: "Failed",
+      message: "3 photos are required",
+    });
+  } else if (!location) {
+    res.json({
+      status: "Failed",
+      message: "Location is required",
+    });
+  } else if (!rate) {
+    res.json({
+      status: "Failed",
+      message: "Amount you charge is required",
+    });
+  } else if (!bio) {
+    res.json({
+      status: "Failed",
+      message: "Bio is required",
+    });
+  } else if (!phoneNumber) {
+    res.json({
+      status: "Failed",
+      message: "Phone number is required",
+    });
+  } else {
+    await User.findOne({ phoneNumber }).then(async (response) => {
+      if (response) {
+        //user found
+        await User.updateOne({
+          phoneNumber,
+          image1,
+          image2,
+          image3,
+          location,
+          rate,
+          bio,
+        })
+
           .then(() => {
             res.json({
               status: "Success",
-              message: "Account created successfully",
+              message: "User updated successfully",
             });
           })
           .catch((err) => {
             console.log(err);
             res.json({
               status: "Failed",
-              message: "Error occured while saving user data",
+              message: "Error occured while updating user",
             });
           });
+      } else {
+        //user not found
+        res.json({
+          status: "Failed",
+          message: "User not found",
+        });
       }
-    })
-    .catch((err) => {
-      console.log(err);
-      req.json({
-        status: "Failed",
-        message: "Error occured while getting user data",
-      });
     });
+  }
 });
 
 module.exports = router;
